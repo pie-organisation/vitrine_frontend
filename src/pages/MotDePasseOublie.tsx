5faa95f854132557a79a1862776b61b0cb2e4599
Talyha <moreau.talyha@gmail.com>
2026-07-20 00:15:12 +0200

    fix: grid correction

--- src/pages/MotDePasseOublie.tsx
+++ src/pages/MotDePasseOublie.tsx
@@ -16,7 +16,7 @@ export function MotDePasseOublie() {
 
   return (
     <div className="min-h-screen flex flex-col">
-      <DotGridBackground />
+      <DotGridBackground grid/>
       <DecorativeOrbs />
 
       <header className="relative z-10 px-8 py-6">
